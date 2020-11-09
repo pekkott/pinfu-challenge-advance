0b3f8f9986ff454ae2db188dbca309545b7bfaf7
pekkott <7554388+pekkott@users.noreply.github.com>
2020-11-10 08:56:22 +0900

    deploy source.

--- mahjong-play-manager/main.go
+++ mahjong-play-manager/main.go
@@ -14,7 +14,6 @@ var addr = flag.String("addr", ":8080", "http service address")
 func main() {
 	hubManager := newHubManager()
 	flag.Parse()
-	http.Handle("/mahjong-ui/", http.StripPrefix("/mahjong-ui/", http.FileServer(http.Dir("../mahjong-ui"))))
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hubManager, w, r)
 	})
