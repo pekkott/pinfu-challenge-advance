26025fdad8776d920a5de881717f609d6084410c
pekkott <7554388+pekkott@users.noreply.github.com>
2020-12-06 20:40:20 +0900

    show start modal.

--- mahjong-ui/js/entry.js
+++ mahjong-ui/js/entry.js
@@ -1,13 +1,10 @@
 class EntryManager {
     constructor() {
-        this.startModal = new StartModal('#modal-start');
-
-        $('#start-free').on('click', (event) => this.openMatchingModal());
+        $('#start-free').on('click', (event) => this.startFree());
         $('#start-set').on('click', (event) => this.startSet());
     }
 
-    openMatchingModal() {
-        this.startModal.showModal();
+    startFree() {
     }
 
     startSet() {
@@ -16,58 +13,6 @@ class EntryManager {
         } else {
             alert("セット名を入力してください");
         }
-    }
-}
-
-class Modal {
-    constructor(modalId) {
-        this.modalId = modalId;
-    }
-
-    showModal() {
-        $(this.modalId).each(function(item, i) {
-            item.classList.add("show-modal");
-        });
-    }
-
-    closeModal() {
-        $(this.modalId).each(function(item, i) {
-            item.classList.remove("show-modal");
-        });
-    }
-}
-
-class StartModal extends Modal {
-    showModal() {
-        super.showModal();
-    }
-}
-
-class WebSocketManager {
-    constructor(mahjongManager) {
-        let self = this;
-        self.mahjongManager = mahjongManager;
-        this.messageHandlers = [
-            {type: "start", handler: this.receiveStart}
-        ];
-        if (window["WebSocket"]) {
-            self.conn = new WebSocket("ws://" + document.location.host + "/ws");
-            self.conn.onmessage = function (evt) {
-                let message = JSON.parse(evt.data);
-                self.messageHandlers.forEach(function(item) {
-                    if (item.type == message["type"]) {
-                        console.log("received message type:" + message["type"]);
-                        item.handler(mahjongManager, message["values"]);
-                    }
-                });
-            }
-        }
-    }
-
-    receiveStart(mahjongManager, playInfo) {
-        console.log(playInfo);
-        mahjongManager.setPlayersId(playInfo.playerIds);
-        mahjongManager.initRound(playInfo);
     }
 }
 
