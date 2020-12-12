5570fc1946f302a146920ef4294513481304fc8c
yuki-sumiya <yuki-sumiya@gmo.jp>
2020-12-12 18:05:58 +0900

    matching apiのlocal環境構築手順を追加

--- app/setting.py
+++ app/setting.py
@@ -3,10 +3,17 @@ from sqlalchemy.orm import *
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-DATABASE = 'mysql://{user}:{password}@{host}/matching_service?charset=utf8'.format(**{
-        'user': os.getenv('MYSQL_USER', 'root'),
-        'password': os.getenv('MYSQL_PASSWORD', 'rootpassword12345hogefuga'),
-        'host': os.getenv('DB_HOST', 'mysql_c'),
+# DATABASE = 'mysql://{user}:{password}@{host}/matching_service?charset=utf8'.format(**{
+#         'user': os.getenv('MYSQL_USER', 'root'),
+#         'password': os.getenv('MYSQL_PASSWORD', 'rootpassword12345hogefuga'),
+#         'host': os.getenv('DB_HOST', 'mysql_c'),
+#     })
+
+DATABASE = 'mysql://{user}:{password}@{host}/{database}?charset=utf8'.format(**{
+        'database' : os.getenv('MYSQL_DATABASE'),
+        'user': os.getenv('MYSQL_USER'),
+        'password': os.getenv('MYSQL_PASSWORD'),
+        'host': os.getenv('DB_HOST'),
     })
 
 ENGINE = create_engine(
